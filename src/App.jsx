8ab5f7f55344250cf1e00adeb55de557500dad5b
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-10-05 15:19:48 +0500

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import "./App.css";
-
 import Busniess from "./components/BusinessCooperation";
 import Footer from "./components/footer";
 import GameSlider from "./components/GameCenter";
-
 import Hero from "./components/Hero";
 import Navvar from "./components/Navvar";
 import SliderComponent from "./components/ProductsSlider";
@@ -14,19 +12,45 @@ import ImageSlider from "./components/TemplateCase";
 
 function App() {
   return (
-    <div className="    ">
+    <div>
       <Navvar />
-      <div className="bg-[url('https://doing.social/img/bg1.b3f24c76.jpg')]">
-        <Hero />
-        <Slider1 />
+      <div>
+        <section
+          id="hero"
+          className="bg-[url('https://doing.social/img/bg1.b3f24c76.jpg')]"
+        >
+          <Hero />
+          <Slider1 />
+        </section>
+
+        <section id="slider">
+          <SliderComponent />
+        </section>
+
+        <section id="games">
+          <GameSlider />
+        </section>
+
+        <section id="card">
+          <CardComponent />
+        </section>
+
+        <section id="service">
+          <Service />
+        </section>
+
+        <section id="image-slider">
+          <ImageSlider />
+        </section>
+
+        <section id="business">
+          <Busniess />
+        </section>
+
+        <section id="footer">
+          <Footer />
+        </section>
       </div>
-      <SliderComponent />
-      <GameSlider />
-      <CardComponent />
-      <Service />
-      <ImageSlider />
-      <Busniess />
-      <Footer />
     </div>
   );
 }
